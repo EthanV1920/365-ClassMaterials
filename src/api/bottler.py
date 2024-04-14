19090f01643cce05b74be1e319bf7ad2b6d0a01e
Ethan Vosburg <63841608+EthanV1920@users.noreply.github.com>
2024-04-14 03:53:58 -0700

    Initial Design for Bottler
    Initial logic and operation for the bottler API call
    Will need to modify later to take care of different bottling combos

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -24,6 +24,51 @@ def post_deliver_bottles(potions_delivered: list[PotionInventory], order_id: int
     """ """
     print(f"potions delivered: {potions_delivered} order_id: {order_id}")
 
+    bottled_potions = [0, 0, 0, 0]
+    updated_ml = [0, 0, 0, 0]
+
+    # TODO: will need to change later to handle mixed potions
+    for potions in potions_delivered:
+        for index, color in enumerate(potions.potion_type):
+            bottled_potions[index] += potions.quantity * color / 100
+
+    sql = sqlalchemy.text("""
+                          SELECT
+                          num_red_ml,
+                          num_green_ml,
+                          num_blue_ml
+                          FROM global_inventory
+                          """)
+
+    update_sql = sqlalchemy.text("""
+                                 UPDATE global_inventory
+                                 SET
+                                 num_red_ml = :red_ml,
+                                 num_green_ml = :green_ml,
+                                 num_blue_ml = :blue_ml,
+                                 num_red_potions = :red_potions,
+                                 num_green_potions = :green_potions,
+                                 num_blue_potions = :blue_potions;
+                                 """)
+
+    # Adding SQL execution
+    with db.engine.begin() as connection:
+        potion_volume = connection.execute(sql).fetchall()
+        print(f"Volume of Potions Before Botteling: {potion_volume}")
+        for index, volume in enumerate(potion_volume[0]):
+            updated_ml[index] = volume - (bottled_potions[index] * 100)
+
+
+    with db.engine.begin() as connection:
+        connection.execute(update_sql, {
+                                 "red_ml": updated_ml[0],
+                                 "green_ml": updated_ml[1],
+                                 "blue_ml": updated_ml[2],
+                                 "red_potions": bottled_potions[0],
+                                 "green_potions": bottled_potions[1],
+                                 "blue_potions": bottled_potions[2]
+                                 })
+
     return "OK"
 
 
@@ -40,28 +85,39 @@ def get_bottle_plan():
     # Initial logic: bottle all barrels into red potions.
 
     sql = sqlalchemy.text("""
-                          SELECT num_green_ml
+                          SELECT
+                          num_red_ml,
+                          num_green_ml,
+                          num_blue_ml
                           FROM global_inventory
                           """)
 
-    update_sql = sqlalchemy.text("""
-                                 UPDATE global_inventory
-                                 SET num_green_ml = :green_ml,
-                                 num_green_potions = :bottled_potions;
-                                 """)
+    # Possible potions
+    potions_to_bottle = [0, 0, 0]
+
     # Adding SQL execution
-    bottled_potions = 0
     with db.engine.begin() as connection:
-        green_ml = connection.execute(sql).scalar()
-        while green_ml >= 100:
-            bottled_potions += 1
-            green_ml -= 100
-        connection.execute(update_sql, {"green_ml": green_ml, "bottled_potions": bottled_potions})
+        potion_volume = connection.execute(sql).fetchall()
+        print(f"Volume of Potions in Inventory: {potion_volume}")
+        for index, volume in enumerate(potion_volume[0]):
+            while volume >= 100:
+                potions_to_bottle[index] += 1
+                volume -= 100
+
+        print(f"Total requested bottles: {potions_to_bottle}")
 
     return [
             {
+                "potion_type": [100, 0, 0, 0],
+                "quantity": potions_to_bottle[0],
+                },
+            {
                 "potion_type": [0, 100, 0, 0],
-                "quantity": 5,
+                "quantity": potions_to_bottle[1],
+                },
+            {
+                "potion_type": [0, 0, 100, 0],
+                "quantity": potions_to_bottle[2],
                 }
             ]
 
